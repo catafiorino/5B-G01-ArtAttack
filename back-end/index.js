b9d8166dbafd8bfbe49929081d6739ae28a77415
FacundoRodrigue <163159203+catafiorino@users.noreply.github.com>
2024-10-16 11:18:57 -0300

    a

--- back-end/index.js
+++ back-end/index.js
@@ -17,7 +17,7 @@ const server = app.listen(LISTEN_PORT, () => {
 
 const io = require('socket.io')(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: ["http://localhost:3000", "http://localhost:3001"],
 		methods: ["GET", "POST", "PUT", "DELETE"],
 		credentials: true
 	}
